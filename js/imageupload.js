273c628f703271bc6cac466844ae9745edbceefb
suyeon kim <88700545+sueyon032@users.noreply.github.com>
2023-07-15 12:42:51 +0900

    image commit

--- js/imageupload.js
+++ js/imageupload.js
@@ -1,29 +1,27 @@
 window.addEventListener('DOMContentLoaded', (event) => {
-    const uploadButton = document.getElementById('uploadButton');
-    const fileInput = document.getElementById('uploadInput');
-    const previewContainer = document.getElementById('previewContainer');
-    const checkImage = document.querySelector('.check-image');  // 이미지 삽입 되었을 때 check-image와 text3 숨기기
-  
-    uploadButton.addEventListener('click', () => {
-      fileInput.click();
-    });
-  
-    fileInput.addEventListener('change', () => {
-      const file = fileInput.files[0];
-      const reader = new FileReader();
-  
-      reader.onload = function (e) {
-        const image = new Image();
-        image.src = e.target.result;
-  
-        previewContainer.innerHTML = '';
-        previewContainer.appendChild(image);
-  
-        // 이미지가 추가될 때 check-image와 text3를 숨김
-        checkImage.style.display = 'none';
-      };
-  
-      reader.readAsDataURL(file);
-    });
+  const uploadButton = document.getElementById('uploadButton');
+  const uploadInput = document.getElementById('uploadInput');
+  const showimage = document.getElementById('showimage');
+  const imageicon = document.querySelector('.imageicon');
+
+  uploadButton.addEventListener('click', () => {
+    uploadInput.click();
   });
-  
+
+  uploadInput.addEventListener('change', () => {
+    const file = uploadInput.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      const image = new Image();
+      image.src = e.target.result;
+
+      showimage.innerHTML = '';
+      showimage.appendChild(image);
+
+      imageicon.style.display = 'none';
+    };
+
+    reader.readAsDataURL(file);
+  });
+});z
